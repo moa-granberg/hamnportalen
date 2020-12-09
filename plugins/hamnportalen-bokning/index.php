9e9b8d1654df362a5478074e837404b30139a6af
moa-granberg <54362217+moa-granberg@users.noreply.github.com>
2020-12-09 15:58:35 +0100

    Booking system styling (#3)
    * init Booking Calendar WpDevArt plugin
    
    * Set up our own booking plugin & importing css
    
    * Change currency to sek & set color to red
    
    * Added fonts
    
    * Change color of available to blue & use css-var
    
    * Copy plugin
    
    * Edit plugin name, translate week days
    
    * Update width
    
    * Add function editWeekDay
    
    * Replace booking-calendar plugin with mod version
    
    * Removed all the previous redundant code JESSICA wrote
    
    * Basic styling for available / unavailable days in calender
    
    * Hide price & availability labels
    
    * Hide booking-legends
    
    * Cursors & hover effect on calendar days
    
    * Borders & paddings calendar
    
    * Change style calendar head
    
    * Bugfix disappering month labels
    
    * Remove the last unwanted borders
    
    * Remove item count input element + labels
    
    * Add styles to submit button
    
    * Edit innertext in form
    
    * Remove item count from checkout form
    
    * Change currency to SEK
    
    * Edit width and alignment on booking form
    
    * Tweaked padding on total span. Moved submit button to center. Removed box shadow from calendar days.
    
    * Altered the link color and the arrows next to the year got some padding
    
    * Added function to render a wrapper around the calendar
    
    * Refact css
    
    * Remove unused function
    
    * Remove allll box-shadows
    
    * All calendar days border-radius
    
    * Bugfix jumping form
    
    * Remake header h1 -> h2
    
    * Font variables
    
    * Style calendar wrapper mobile & desktop
    
    * Shadows and rounded corners
    
    * Add styles for past day in calendar
    
    * Toggle calendar/booking section
    
    * Add animation on calendar section
    
    * Bug fix on item-count
    
    * Calendar fixed at bottom in mobile mode
    
    * Fixed so calender is shown in admin view
    
    * Length fix on calendar
    
    Co-authored-by: Jessica <jessica.schwabegger@hotmail.com>
    Co-authored-by: olle-forsslof <54362382+olle-forsslof@users.noreply.github.com>

---
+++ plugins/hamnportalen-bokning/index.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+Plugin Name: Hamnportalen Bokning
+*/
+
+add_action('wp_loaded', 'booking_override');
+
+function booking_override() {
+  $plugin_url = plugin_dir_url(__FILE__);
+  //importing styles
+  wp_register_style('bookingStyle', $plugin_url . '/bookingStyle.css');
+  wp_enqueue_style('bookingStyle');
+  wp_register_style('bookingStyleCalendar', $plugin_url . '/bookingStyleCalendar.css');
+  wp_enqueue_style('bookingStyleCalendar');
+  wp_register_style('bookingStyleForm', $plugin_url . '/bookingStyleForm.css');
+  wp_enqueue_style('bookingStyleForm');
+
+  //importing scripts
+  wp_register_script('bookingScript', $plugin_url . '/bookingScript.js', [], false, true);
+  wp_enqueue_script('bookingScript');
+
+  //importing fonts
+  wp_enqueue_style( 'google-fonts-montserrat', 'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap', false);
+  wp_enqueue_style( 'google-fonts-dosis', 'https://fonts.googleapis.com/css2?family=Dosis:wght@400;600&display=swap', false);
+}
