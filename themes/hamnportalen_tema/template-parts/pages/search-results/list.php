3acdaa1b4f6b4ddaabff7e43161034d9039001dd
Moa Granberg <moa.granberg@hotmail.com>
2021-01-12 10:50:39 +0100

    SQL-query based on slug & saved values in variables

--- themes/hamnportalen_tema/template-parts/pages/search-results/list.php
+++ themes/hamnportalen_tema/template-parts/pages/search-results/list.php
@@ -1,2 +1,25 @@
-<?php 
-echo 'list';
+<?php
+if ($_GET['search_params']) {
+  $search_query = substr($_GET['search_params'], 0, -1);
+
+  global $wpdb;
+  $table_name = $wpdb->prefix . "posts";
+
+  $results = $wpdb->get_results(
+    "SELECT * FROM $table_name 
+    WHERE post_title LIKE '$search_query%'
+    AND post_status = 'publish'");
+  
+  foreach($results as $port) {
+    //Values
+    $port_name = $port->post_title;
+    $port_price = get_field('price', $port->ID);
+    $port_image1 = get_field('hero_img_1', $port->ID);
+    $port_facilities = get_field('facilities', $port->ID);
+
+    //Booleans
+    $port_restaurant = array_search('Restaurang', $port_facilities);
+    $port_bath = array_search('Badplats', $port_facilities);
+    $port_internet = array_search('Internet', $port_facilities);
+  }
+}
