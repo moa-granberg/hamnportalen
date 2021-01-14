22047ba7ee3441efb0549d7a1ff1761dc3f91da3
Jessica <jessica.schwabegger@hotmail.com>
2021-01-14 10:27:23 +0100

    Set up toggle button

--- themes/hamnportalen_tema/template-parts/pages/search-results/search-results.php
+++ themes/hamnportalen_tema/template-parts/pages/search-results/search-results.php
@@ -1,4 +1,25 @@
+<?php
+$listClass = "show-search-button";
+$mapClass = "hide-search-button";
+
+if (isset($_POST["map"])) {
+  $mapClass = "show-search-button";
+  $listClass = "hide-search-button";
+}
+if (isset($_POST["list"])) {
+  $listClass = "show-search-button";
+  $mapClass = "hide-search-button";
+}
+
+?>
 <section class="search-results-main-wrapper">
   <?php require_once(__DIR__ . '/map.php'); ?>
   <?php require_once(__DIR__ . '/list.php'); ?>
 </section>
+
+<form class="<?php echo $listClass ?>" action="" method="POST">
+  <input type="submit" value="Karta" name="map">
+</form>
+<form class="<?php echo $mapClass ?>" action="" method="POST">
+  <input type="submit" value="Lista" name="list">
+</form>
