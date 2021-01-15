29d6fd3eef18786393761cb980f56a01c3b24e05
iths-kim-lindgren <48940807+iths-kim-lindgren@users.noreply.github.com>
2021-01-15 10:37:50 +0100

    header with dropdown icons (#5)
    * header with dropdown icons
    
    * toggle nav fungerar, styling återstår
    
    * header utan sökfält klar
    
    * header utan sökfält
    
    * uppercase och hover på länkar, vit rand i footern borttagen, header-rubrik syns på alla pages
    
    * uppdaterat hovereffekter
    
    * removed themes
    
    * removed hello.php
    
    * raderat överflödig 2020-fil och ominstallerat node_modules
    
    * deleted unused files, fixed previous error
    
    * modified node-sass installation
    
    * fixat logik för rendering startsida/pages med php
    
    * åtgärdat kommentarerna på pull request, villkorad rendering av landningssidan återstår att lösa
    
    * rendering av landningssida fungerar
    
    Co-authored-by: Alexander Rusiecki <alexander.rusiecki@gmail.com>

--- themes/hamnportalen_tema/header.php
+++ themes/hamnportalen_tema/header.php
@@ -24,36 +24,82 @@
 <?php wp_body_open(); ?>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'hamnportalen_tema' ); ?></a>
-
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+		<header id="masthead" class="site-header">
+			<div class="site-branding">
 				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$hamnportalen_tema_description = get_bloginfo( 'description', 'display' );
-			if ( $hamnportalen_tema_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $hamnportalen_tema_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+				the_custom_logo();
+				if ( is_front_page()  ) :
+						?>
+					<!-- <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p> -->
+					<?php
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'hamnportalen_tema' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+        else :
+          ?>
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<?php
+				
+				endif;
+		
+			  	?>
+					<!-- <p class="site-description"><?php // echo $hamnportalen_tema_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p> -->
+				<!-- <?php // endif; ?> -->
+			</div><!-- .site-branding -->
+
+			<nav id="site-navigation" class="main-navigation">
+				
+				<!-- <img class="menu-toggle" src="<?php echo get_theme_file_uri('/assets/images/hamburger-menu.png'); ?>" alt="menu">  -->
+				<button class="menu-toggle hamburger-menu" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( '', 'hamnportalen_tema' ); ?></button>
+				<?php
+				wp_nav_menu(
+					array(
+						'theme_location' => 'menu-1',
+						'menu_id'        => 'primary-menu',
+					)
+				);
+				?>
+
+      </nav><!-- #site-navigation -->
+		</header><!-- #masthead -->
+
+
+<script>
+	let menu = document.querySelector("div#primary-menu")
+	let ul = menu.firstElementChild
+	let button = document.createElement("button")
+	ul.appendChild(button)
+	button.classList.add("secondary_btn")
+	button.innerText = "LOGGA IN"
+
+	let menuButton = document.querySelector("button.menu-toggle")
+	let links = document.querySelector(".menu")
+
+	menuButton.addEventListener("click", () => {
+		menuButton.classList.toggle("cancel-cross")
+		let dropdown = document.querySelector("header.site-header")
+		dropdown.classList.toggle("dropdown")
+		links.classList.toggle("show-links")
+	});
+
+	<?php if ( is_front_page()  ) :	?>
+
+		document.querySelector("div.site-branding").style.display = "none"
+
+		const list = document.querySelectorAll('li.page_item');
+		for (let li of list) {
+			li.style.display = "none";
+		}
+		menuButton.style.display = "none";
+		links.classList.add("show-links")
+		document.querySelector(".secondary_btn").classList.add("landingPageMobileButton")
+		document.querySelector(".secondary_btn").style.marginTop = "0";
+		
+		let header = document.querySelector("header.site-header")
+		header.classList.add("landingPageHeader")
+		
+		let h1 = document.createElement("h1")
+		h1.classList.add("landingPageH1")
+		h1.textContent ="HAMNPORTALEN"
+		header.appendChild(h1)
+
+	<?php endif;?>
+</script>
