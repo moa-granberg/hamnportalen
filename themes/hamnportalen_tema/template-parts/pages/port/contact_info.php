f07406d32aced8f1fa687dbec5e2acec0627ce48
Jessica <jessica.schwabegger@hotmail.com>
2021-01-19 12:13:57 +0100

    Render content in contact information section

--- themes/hamnportalen_tema/template-parts/pages/port/contact_info.php
+++ themes/hamnportalen_tema/template-parts/pages/port/contact_info.php
@@ -2,5 +2,9 @@
   <div class="port-article__header">
     <h2>Kontaktinformation</h2>
     <hr/>
+    <p>Adress: <?php echo $portData['address']; ?></p>
+    <p>Telefon: <?php echo $portData['phone']; ?></p>
+    <p>E-post: <?php echo $portData['email']; ?></p>
   </div>
 </article>
+
