cd3f7cfaaabc055bc7f00de84352b06239bed155
Jessica <jessica.schwabegger@hotmail.com>
2021-01-19 12:31:35 +0100

    Set up scss file for contact-info and add styles

--- themes/hamnportalen_tema/template-parts/pages/port/contact_info.php
+++ themes/hamnportalen_tema/template-parts/pages/port/contact_info.php
@@ -2,9 +2,11 @@
   <div class="port-article__header">
     <h2>Kontaktinformation</h2>
     <hr/>
-    <p>Adress: <?php echo $portData['address']; ?></p>
-    <p>Telefon: <?php echo $portData['phone']; ?></p>
-    <p>E-post: <?php echo $portData['email']; ?></p>
+    <div class="contact-info-wrapper">
+      <p><span>Adress:</span> <?php echo $portData['address']; ?></p>
+      <p><span>Telefon:</span> <?php echo $portData['phone']; ?></p>
+      <p><span>E-post:</span> <?php echo $portData['email']; ?></p>
+    </div>
   </div>
 </article>
 
