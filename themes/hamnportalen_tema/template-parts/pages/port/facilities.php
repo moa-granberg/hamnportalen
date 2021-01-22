0f52bb876b85aefab2756ef499d80ec6b9111366
moa-granberg <54362217+moa-granberg@users.noreply.github.com>
2021-01-22 13:23:50 +0100

    Port facilities (#17)
    * Set up renderFacilties function in loop through portData's facilities
    
    * Render all possible facilities with svg showing if it's available at this port
    
    * Style facilities mobile & desktop

--- themes/hamnportalen_tema/template-parts/pages/port/facilities.php
+++ themes/hamnportalen_tema/template-parts/pages/port/facilities.php
@@ -1,6 +1,62 @@
+<?php 
+ $allFacilities = [
+  "Diesel",
+  "Bensin",
+  "Gasol",
+  "Färskvatten",
+  "Toalett",
+  "Dusch",
+  "Bastu",
+  "Tvättstuga",
+  "Internet",
+  "Eluttag",
+  "Trailerramp",
+  "Miljöstation",
+  "Latrinvask",
+  "Septiksug",
+  "Kran/Truck",
+  "Mastkran",
+  "Båtvarv",
+  "Motorservice",
+  "Restaurang",
+  "Badplats",
+  "Livsmedel",
+  "Handikappsanpassat",
+  "Vinteröppet",
+  "Ställplatser",
+  "Läkare",
+  "Apotek",
+  "Tandläkare",
+  "Veterinär",
+  "Turistbyrå",
+  "Buss/Tåg/Färja" 
+  ];
+  
+  function renderFacility($facility, $portFacilities) {
+    $imageUri = in_array($facility, $portFacilities) ? 
+      get_theme_file_uri('/assets/images/check.svg') : 
+      get_theme_file_uri('/assets/images/cross.svg');
+    
+    return "
+      <div class='port-facilities__facility'>
+        <img src='$imageUri' />
+        <p>$facility</p>
+      </div>
+    ";
+  }
+?>
+
 <article class="port-facilities port-article">
   <div class="port-article__header">
     <h2>Faciliteter</h2>
     <hr/>
+  </div>
+
+  <div class="port-facilities__facilities-wrapper">
+    <?php 
+    foreach ($allFacilities as $facility) { 
+      echo renderFacility($facility, $portData['facilities']);
+    }
+    ?>
   </div>
 </article>
