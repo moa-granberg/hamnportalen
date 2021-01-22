859e89cf038cd337b4ff733624bf975c8ab8fbe5
moa-granberg <54362217+moa-granberg@users.noreply.github.com>
2021-01-22 13:50:03 +0100

    Port map and road (#19)
    * Render road description from port metadata
    
    * Render map centered on port
    
    * Fixed height & width of port-map article
    
    * Add p-tag to wrap road-description
    
    * Switch import of env file to fix bug

--- themes/hamnportalen_tema/template-parts/pages/port/map.php
+++ themes/hamnportalen_tema/template-parts/pages/port/map.php
@@ -1,1 +1,33 @@
-<article class="port-map port-article">this is map</article>
+<?php
+  require_once(realpath( __DIR__ ).'/../search-results/.env.php' );
+?>
+
+<article class="port-map port-article" data-value="<?php echo $portData['name'] ?>"></article>
+
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=<?php echo $apiKey ?>&callback=initMap" type="text/javascript"></script>
+<script async type="text/javascript">
+
+  const mapContainer = document.querySelector('.port-map');
+
+  const getPort = async (portName) => {
+    const url = `${window.location.href.split('/blog/')[0]}/wp-json/mapsearch/search/${portName}/`;
+    const results = await fetch(url).then(response => response.json());
+    return results[0];
+  }
+
+  async function initMap() {  
+    const port = await getPort(mapContainer.getAttribute('data-value'));
+
+    const map = new google.maps.Map(mapContainer, {
+      zoom: 10,
+      center: { lat: port.lat, lng: port.long },
+    });
+
+    new google.maps.Marker({
+      position: { lat: port.lat, lng: port.long },
+      icon: 'https://maps.google.com/mapfiles/ms/micons/marina.png',
+      map
+    });
+  }
+
+</script>
--- themes/hamnportalen_tema/template-parts/pages/port/road_description.php
+++ themes/hamnportalen_tema/template-parts/pages/port/road_description.php
@@ -3,4 +3,9 @@
     <h2>Vägbeskrivning</h2>
     <hr/>
   </div>
+  <div>
+    <p>
+      <?php echo $portData['road_description']?>
+    </p>
+  </div>
 </article>
