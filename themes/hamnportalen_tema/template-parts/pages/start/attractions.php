e0d477e067b431818749f49c210d4e292ddfd1e7
moa-granberg <54362217+moa-granberg@users.noreply.github.com>
2021-01-25 14:35:50 +0100

    Start attractions (#28)
    * Fetch data & render in loop
    
    * Add background-image
    
    * Render attraction-card html with correct content
    
    * Add space between port titles
    
    * Style mobile & desktop
    
    * Adjust attraction-card's margin on mobile
    
    * Add box-shadow to attractions-cards

--- themes/hamnportalen_tema/template-parts/pages/start/attractions.php
+++ themes/hamnportalen_tema/template-parts/pages/start/attractions.php
@@ -1,5 +1,50 @@
-<article class="start-events start-article">
+<?php
+  global $wpdb;
+  $table_name = $wpdb->prefix . 'posts';
+
+  $attractions = $wpdb->get_results(
+    "SELECT * FROM $table_name
+    WHERE post_type = 'attraction'
+    AND post_status = 'publish'
+    LIMIT 4"
+  );
+
+  function get_close_ports_string ($ports) {
+    $port_titles = [];
+
+    foreach($ports as $port) {
+      array_push($port_titles, $port->post_title);
+    }
+
+    return implode(', ', $port_titles);
+  }
+
+?>
+
+<article class="start-attractions start-article">
   <div class="start-article__header">
     <h2>Sevärdheter</h2>
+  </div>
+
+  <div class="start-attractions__content-wrapper">
+    <?php
+      foreach($attractions as $attraction) {
+        $attraction_data = get_fields($attraction->ID);
+    ?>
+      <div class="attraction-card">
+        <img src="<?php echo $attraction_data['img_url'] ?>"/>
+        <div class="attraction-card__text-wrapper">
+          <div class="attraction-card__heading">
+            <h3><?php echo $attraction->post_title ?></h3>
+          </div>
+          <div class="attraction-card__inner-text-wrapper">
+            <h4><?php echo get_close_ports_string($attraction_data['ports']) ?></h4>
+            <p><?php echo $attraction_data['description'] ?></p>
+          </div>
+        </div>
+      </div>
+    <?php
+      }
+    ?>
   </div>
 </article>
