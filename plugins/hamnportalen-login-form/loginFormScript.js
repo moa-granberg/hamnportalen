22b07ccb444af2d05173ca6359f79b462dc843ec
iths-jessica-schwabegger <54362390+iths-jessica-schwabegger@users.noreply.github.com>
2021-01-25 14:17:06 +0100

    Bug fix login form (#30)
    * Remove console log
    
    * Add if-statement to updateInputType function

--- plugins/hamnportalen-login-form/loginFormScript.js
+++ plugins/hamnportalen-login-form/loginFormScript.js
@@ -1,7 +1,10 @@
 //Change input type from "text" to "password" in  login form.
-console.log("Hej");
 const updateInputType = () => {
-    document.querySelector("#wpforms-202-field_4").type = 'password';
+    const loginInputField = document.querySelector("#wpforms-202-field_4")
+    
+    if(loginInputField) {
+        loginInputField.type = 'password';
+    }
 }
 
 updateInputType();
