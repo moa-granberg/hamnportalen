b1ebe53df739842b90040e11096792ed25a79e94
Olle Forsslöf <54362382+olle-forsslof@users.noreply.github.com>
2021-01-26 11:07:16 +0100

    Start adjustments (#33)
    * Refactoring some css
    
    * Padding adjustment and made sure that there are no disabled scroll bars when in mobile view.
    
    * Event cards now act like the attraction cards in mobile view. Adjusted margins and padding to be continous all over.
    
    * Css styles
    
    * Removed comments

--- themes/hamnportalen_tema/template-parts/pages/start/events.php
+++ themes/hamnportalen_tema/template-parts/pages/start/events.php
@@ -13,28 +13,29 @@
 <article class="start-events start-article">
   <div class="start-article__header">
     <h2>Kommande event</h2>
-    <article class="hp-event-wrapper">
-      <?php
-  
+  </div>
+
+  <article class="hp-event-wrapper">
+  <?php
     foreach ($results as $event){
       $event_id = $event->ID;
       $data = get_fields( $event_id );
       $port_url = $data['ports'][0]->guid;
-    ?>
-      <a href="<?php echo $port_url; ?>" class="hp-event-card-link">
-        <article class="hp-event-card hp-event__wrapper">
-          <div id="hp-event__info-wrapper">
-            <h3><?php echo $data['ports'][0]->post_title ?></h3>
-            <h4 class="hp-event-h4"><?php echo $data['title'] ?></h4>
-            <p class="hp-event-date"><?php echo $data['date'] . '  |  ' . $data['time'];?></p>
-            <div class="hp-event-description lineclamp"><p><?php echo $data['description'];?></p></div>
-          </div>
-          <div class="hp-event__img-wrapper">
-            <img src="<?php echo $data['img_url']; ?>" alt="">
-          </div>
-        </article>
-      </a>
-    <?php };?>
-    </article>
-  </div>
+  ?>
+    <a href="<?php echo $port_url; ?>" class="hp-event-card-link">
+      <article class="hp-event-card hp-event__wrapper">
+        <div id="hp-event__info-wrapper">
+          <h3><?php echo $data['ports'][0]->post_title ?></h3>
+          <h4 class="hp-event-h4"><?php echo $data['title'] ?></h4>
+          <p class="hp-event-date"><?php echo $data['date'] . '  |  ' . $data['time'];?></p>
+          <div class="hp-event-description lineclamp"><p><?php echo $data['description'];?></p></div>
+        </div>
+        <div class="hp-event__img-wrapper">
+          <img src="<?php echo $data['img_url']; ?>" alt="">
+        </div>
+      </article>
+    </a>
+  <?php };?>
+  </article>
+  
 </article>
