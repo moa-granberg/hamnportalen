73dddcc370dba9c9032df49a27f245d6f2c0721a
Alexander Rusiecki <54362381+iths-alexander-rusiecki@users.noreply.github.com>
2021-01-26 09:28:49 +0100

    Port personnel (#21)
    * adds 2 new personnels to Bjuröklubb port, renders 1 personnel in own div
    
    * iterates through fields object and puts names + image urls into variables + echoes template
    
    * gets fields with  + adds personnel.scss + adds some styling to personnel-card
    
    * h3 changed to h2 + some grid styling
    
    * adds card wrapper + changes h2-tag to p-tag + adds desktop media queries
    
    * deletetes hardcoded 'hamnansvarig' + deletes 2 unnecessary divs + some changes to css

--- themes/hamnportalen_tema/template-parts/pages/port/personnel.php
+++ themes/hamnportalen_tema/template-parts/pages/port/personnel.php
@@ -1,6 +1,35 @@
 <article class="port-personnel port-article">
   <div class="port-article__header">
     <h2>Vi som jobbar här</h2>
-    <hr/>
+    <hr/>    
   </div>
+
+    <?php
+
+    $staffOneName = $portData["staff_name_1"];
+    $staffTwoName = $portData["staff_name_2"];
+    $staffThreeName = $portData["staff_name_3"];
+    $staffOneImageUrl = $portData["staff_img_1"];
+    $staffTwoImageUrl = $portData["staff_img_2"];
+    $staffThreeImageUrl = $portData["staff_img_3"];
+    
+    echo "
+    <div class='personnel-card-wrapper'>
+      <div class='personnel-card'>
+        <img src='$staffOneImageUrl'>
+        <h4>$staffOneName</h4>
+      </div>
+      <div class='personnel-card'>
+        <img src='$staffTwoImageUrl'>
+        <h4>$staffTwoName</h4>
+      </div>
+      <div class='personnel-card'>
+        <img src='$staffThreeImageUrl'>
+        <h4>$staffThreeName</h4>
+      </div>
+    </div>
+    "
+  
+    ?>
 </article>
+
