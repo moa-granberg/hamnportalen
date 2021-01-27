a1faddf8503f5c444d0aa804b180932354d8c4de
Alexander Rusiecki <54362381+iths-alexander-rusiecki@users.noreply.github.com>
2021-01-27 09:15:48 +0100

    Port hero image slide (#36)
    * initial test commit
    
    * adds html + adds arrow svg + some basic styling
    
    * adds slide functionality but without actual port image amount
    
    * adds fade animation + gets image urls dynamically + renders images dynamically
    
    * correct img alt text
    
    * changes css, not finished
    
    * adds better styling to both mobile and desktop + still some styling to be done
    
    * centers arrows + changes arrow size
    
    * deletes 100vw

--- themes/hamnportalen_tema/template-parts/pages/port/hero.php
+++ themes/hamnportalen_tema/template-parts/pages/port/hero.php
@@ -1,1 +1,87 @@
-<article class="port-hero port-article">hero</article>
+<article class="port-hero port-article">
+  <div class="slider-container">
+
+  <?php
+  
+  $arrowLeftUrl = get_theme_file_uri('/assets/images/arrow-left-white.svg');
+  $arrowRightUrl = get_theme_file_uri('/assets/images/arrow-right-white.svg');
+  $fields = get_field_objects();
+
+  echo "
+  <div id='arrow-left' class='arrow'>
+    <img src=$arrowLeftUrl alt='arrow left'>
+  </div>
+  ";
+  
+  foreach ($fields as $field) {
+    $label = $field["label"];
+    $imageUrl = $field["value"];
+    if (strpos($label, 'Hero IMG') === 0 && (gettype($imageUrl) === "string")) {
+      echo "
+        <div class='slide'>
+          <div class='slide-content'>
+            <img src=$imageUrl alt='port image'>
+          </div>
+        </div>
+      ";
+    }
+  }
+
+  echo "
+  <div id='arrow-right' class='arrow'>
+    <img src=$arrowRightUrl alt='arrow right'>
+  </div>
+  ";
+  
+  ?>
+  
+  </div>
+</article>
+
+<script>
+
+const sliderImages = document.querySelectorAll(".slide");
+const arrowRight = document.querySelector("#arrow-right");
+const arrowLeft = document.querySelector("#arrow-left");
+let current = 0;
+
+const reset = () => {
+  for(let i = 0; i < sliderImages.length; i++) {
+    sliderImages[i].style.display = "none";
+  }
+}
+
+const startSlide = () => {
+  reset();
+  sliderImages[0].style.display = "block";
+}
+
+const slideLeft = () => {
+  reset();
+  sliderImages[current - 1].style.display = "block";
+  current--;
+}
+
+const slideRight = () => {
+  reset();
+  sliderImages[current + 1].style.display = "block";
+  current++;
+}
+
+arrowLeft.addEventListener("click", () => {
+  if(current === 0) {
+    current = sliderImages.length;
+  }
+  slideLeft();
+})
+
+arrowRight.addEventListener("click", () => {
+  if(current === sliderImages.length - 1) {
+    current = -1;
+  }
+  slideRight();
+})
+
+startSlide();
+
+</script>
